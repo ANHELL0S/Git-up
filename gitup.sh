dccc474cf078ae9ec862f8c1152e9f8506320b34
Angelo García <161162882+ANHELL0S@users.noreply.github.com>
2024-11-21 23:45:36 -0500

    Update gitup.sh

--- gitup.sh
+++ gitup.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
-# Establecer colores
+# Establecer el color
 reset="\033[0m"
-green="\033[0;32m"
-blue="\033[0;34m"
-yellow="\033[0;93m"
-red="\033[0;31m"
+green="\033[0;32m"    
+blue="\033[0;34m"      
+yellow="\033[0;93m"    
+red="\033[0;31m"       
+purple="\033[0;35m" 
 
-# Iconos
 pin="✍️"
+brach="🔎"
+summary="📢"
+check_mark="🔨"
 warning="🤬"
+push_done="🚀"
+wait="🙏"
+thinking="🤔"
+fire="🔥"
+push="💾"
 
 # Verificar si es un repositorio de Git
 if ! git rev-parse --is-inside-work-tree &>/dev/null; then
@@ -17,36 +25,35 @@ if ! git rev-parse --is-inside-work-tree &>/dev/null; then
     exit 1
 fi
 
-# Ingresar el mensaje de commit
-echo -e "${pin} ${blue}Enter your commit description:${reset} \c"
+# Ingresar el mensaje de commit en la misma línea
+echo -e "${pin} ${blue} Enter your commit description:${reset} \c"
 read commit_message
 
 # Agregar cambios al staging area
-echo -e "\n${yellow}Staging the changes...${reset}"
+echo -e "\n${wait} ${yellow}Hang on, we're staging the changes...${reset}"
 git add .
 
 # Mostrar resumen de los cambios
-echo -e "\n${blue}Here's a quick look at your changes:${reset}\n"
+echo -e "\n${pin} ${blue} Here's a quick look at your changes:${reset}\n"
 git status -s
 
 # Confirmar si se quiere hacer el commit
-branch_name=$(git branch --show-current)
-echo -e "\n${blue}You're currently on branch --> ${green}${branch_name}${reset}\n"
-echo -e "Ready to commit these changes? (y/n): \c"
+echo -e "\n${brach} ${blue}You're currently on branch --> ${green}$(git branch --show-current)${reset}\n"
+echo -e "${thinking} ${yellow}Ready to commit these changes? (y/n):${reset} \c"
 read confirm_commit
 
 if [[ "$confirm_commit" == "y" || "$confirm_commit" == "Y" ]]; then
     # Realizar commit
-    echo -e "\n${green}Committing your changes...${reset}"
+    echo -e "\n${check_mark} ${green}Great! We're committing your changes...${reset}\n"
     git commit -m "$commit_message"
 
-    # Hacer push
-    echo -e "\n${blue}Pushing the updates to the server...${reset}"
+    # Verificar si hay cambios para hacer push
+    echo -e "\n${push} ${blue}Pushing the updates to the server...${reset}\n"
     git push
 
     # Mensaje final
-    echo -e "\n${green}All done! Your changes have been uploaded successfully! 🎉${reset}"
+    echo -e "\n${push_done} ${green}All done! Your changes have been uploaded successfully! 🎉${reset}\n"
 else
     # Si se cancela el commit
-    echo -e "\n${red}Commit canceled. Try again later.${reset}"
+    echo -e "\n${warning} ${red}Oops! Commit canceled. No worries, try again later. 😅${reset}\n"
 fi
